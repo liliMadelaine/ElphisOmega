0db5d28d19bde5929c067d26ba10df605ecfadf3
Katu603 <k12017259@students.jku.at>
2021-06-12 23:12:15 +0200

    scripts_for_sfx_added
    TODO: audio files need to be added

---
+++ ElphisΩ/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    
+    void Awake(){
+        foreach(Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            float volume = (PlayerPrefs.GetFloat("VideoVolume") + 20)/ 40;
+            s.source.volume = volume;
+            s.source.loop = s.loop;
+
+            /*
+            to call within the code at desired point LATER
+            FindObjectOfType<AudioManager>().Play("PlayerDeath");
+            */
+        }
+    }
+
+    public void Play (string title){
+        Sound s = Array.Find(sounds, sound => sound.title == title);
+        if(s == null){
+            Debug.LogWarning("Sound wasn't found.");
+            return;
+        }
+        s.source.Play();
+    } 
+}
